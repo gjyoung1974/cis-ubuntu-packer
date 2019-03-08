c3f5282a69e1816b9e5e35e7aa301648bbad96c7
Gordon Young <gjyoung1974@gmail.com>
2019-03-08 15:29:22 -0700

    much refactoring

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # cis-ubuntu-packer
-CIS Hardened Ubuntu Docker Image
-
-### Authenticate / get a JWT token for Gcloud
-`gcloud auth application-default login`
-
-### Pack a CIS hardened Ubuntu Image
-`packer build ./packer.json`
+### What's this? 
+####  This project packs a "Center for Itnernet Security" benchmark hardened Ubuntu image for Google Compute Engine
+`CIS Hardened Ubuntu Image`
+1. Edit the file: packer.json
+1. Authenticate / get a JWT token for Gcloud
+```sh
+gcloud auth application-default login
+```
+1. Pack a CIS hardened Ubuntu Image
+```sh
+packer build ./packer.json
+```
 
 ---
 2019 Gordon Young - gjyoung1974@gmail.com
--- builder/platform-uploads/setup-scripts/05-acme-ssh-ca.sh
+++ builder/platform-uploads/setup-scripts/05-acme-ssh-ca.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -xe
-
-# scalesec-ssh-ca
-# sudo cp /tmp/scalesec-cas.pub /etc/ssh/scalesec-cas.pub
-sudo mkdir -p /etc/ssh/auth_principals/
-sudo sh -c 'echo "TrustedUserCAKeys /etc/ssh/scalesec-cas.pub" >> /etc/ssh/sshd_config'
-sudo sh -c 'echo "AuthorizedPrincipalsFile /etc/ssh/auth_principals/%u" >> /etc/ssh/sshd_config'
-sudo sh -c 'echo "scalesec-recovery-user" > /etc/ssh/auth_principals/scalesec-recovery-user'
-sudo sh -c 'echo "scalesec-recovery-user ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/scalesec-recovery-user'
-sudo useradd -m scalesec-recovery-user -s /bin/bash
-sudo -H -u scalesec-recovery-user bash -c 'mkdir ~/.ssh'
-# sudo -H -u scalesec-recovery-user bash -c 'cat /tmp/scalesec-recovery-user.pub > ~/.ssh/authorized_keys'
-# sudo chmod 600 /home/scalesec-recovery-user/.ssh/authorized_keys
+# What's this? 
+# Customize the SSH Daemon
+# acme-ssh-ca
+# sudo cp /tmp/acme-cas.pub /etc/ssh/acme-cas.pub
+# sudo mkdir -p /etc/ssh/auth_principals/
+# sudo sh -c 'echo "TrustedUserCAKeys /etc/ssh/acme-cas.pub" >> /etc/ssh/sshd_config'
+# sudo sh -c 'echo "AuthorizedPrincipalsFile /etc/ssh/auth_principals/%u" >> /etc/ssh/sshd_config'
+# sudo sh -c 'echo "acme-recovery-user" > /etc/ssh/auth_principals/acme-recovery-user'
+# sudo sh -c 'echo "acme-recovery-user ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/acme-recovery-user'
+# sudo useradd -m acme-recovery-user -s /bin/bash
+# sudo -H -u acme-recovery-user bash -c 'mkdir ~/.ssh'
+# # sudo -H -u acme-recovery-user bash -c 'cat /tmp/acme-recovery-user.pub > ~/.ssh/authorized_keys'
+# # sudo chmod 600 /home/acme-recovery-user/.ssh/authorized_keys
